5ae79382babf4a7c1228f0ef1f63ca8e07918cdf
KIMYOUNGHAN <yh22953905@gmail.com>
2021-05-08 18:58:18 +0900

    스프링 기반 REST API 개발 - 테스트용 DB와 설정 분리하기

--- src/test/java/me/kimyounghan/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/kimyounghan/restapiwithspring/events/EventControllerTests.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTests {
 
     @Autowired
