c6502705155023262fe1625b4f820995c924736f
KIMYOUNGHAN <yh22953905@gmail.com>
2021-05-15 23:06:45 +0900

    스프링 기반 REST API 개발 - 스프링 시큐리티 적용

---
+++ src/test/java/me/kimyounghan/restapiwithspring/accounts/AccountServiceTest.java
@@ -0,0 +1,49 @@
+package me.kimyounghan.restapiwithspring.accounts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void findByUsername() {
+        // Given
+        String username = "email@email.com";
+        String password = "password";
+
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+
+        accountRepository.save(account);
+
+        // When
+        UserDetailsService userDetailsService = (UserDetailsService) accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        // Then
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+}
