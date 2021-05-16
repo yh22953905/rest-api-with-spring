e755075bac8acee58ea396dc65bb73fd37475907
KIMYOUNGHAN <yh22953905@gmail.com>
2021-05-16 23:34:39 +0900

    스프링 기반 REST API 개발 - 예외 테스트

--- src/test/java/me/kimyounghan/restapiwithspring/accounts/AccountServiceTest.java
+++ src/test/java/me/kimyounghan/restapiwithspring/accounts/AccountServiceTest.java
@@ -1,22 +1,31 @@
 package me.kimyounghan.restapiwithspring.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -44,6 +53,31 @@ public class AccountServiceTest {
 
         // Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+//    @Test(expected = UsernameNotFoundException.class) // 발생할 예외의 타입을 확인할 수 있음.
+    @Test
+    public void findByUsernameFail() {
+        String username = "notFoundUsername";
+
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");
+        } catch (UsernameNotFoundException e) {
+//            assertThat(e instanceof UsernameNotFoundException).isTrue();
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+    }
+
+    @Test
+    public void findByUsernameFail2() {
+        // Expected
+        String username = "notFoundUsername";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
     }
 
 }
